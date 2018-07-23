b5b132b412362e7962347cb3ce676db8f7278cc1
amaurs <amaury.gtz@gmail.com>
2018-07-23 16:43:55 -0500

    Added banners.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Responsive from 'react-responsive';
+
+const Desktop = props => <Responsive {...props} minWidth={992} />;
+//const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
+//const Mobile = props => <Responsive {...props} maxWidth={767} />;
+//const Default = props => <Responsive {...props} minWidth={768} />;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App-container">
+        <div className="App-info">
+          <h1>Reporte ANP</h1>
+        </div>
+        <Desktop>
+          <div className="App-list">
+            <ul>
+              <li>Desierto de los leones</li>
+              <li>Cañón del Sumidero</li>
+              <li>Mineral del chico</li>
+            </ul>
+          </div>
+        </Desktop>
       </div>
     );
   }
