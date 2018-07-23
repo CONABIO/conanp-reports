42a4e4945ec762c9f916b77565ac449a65a01929
amaurs <amaury.gtz@gmail.com>
2018-07-23 18:24:34 -0500

    Added the map and the geojson layer.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 import Responsive from 'react-responsive';
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 
 const Desktop = props => <Responsive {...props} minWidth={992} />;
 //const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
 //const Mobile = props => <Responsive {...props} maxWidth={767} />;
 //const Default = props => <Responsive {...props} minWidth={768} />;
+const position = [23.950464, -102.532867];
+const zoom = 5;
+const API = "http://127.0.0.1:8080/anps.geojson";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      geojson: null,
+      ready:false
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        this.setState({geojson:data,
+                       ready:true});
+      });
+  }
+
+  getGeoJson() {
+    return this.state.geojson;
+  }
+
+  getStyle(feature, layer) {
+    return {
+      color: '#006400',
+      weight: 5,
+      opacity: 0.65
+    }
+  }
+
+  getList() {
+    let slice = this.state.geojson;
+  }
+
   render() {
+    let geom = null;
+    if (this.state.ready) {
+      console.log("I am ready to paint!");
+      geom = <GeoJSON data={this.getGeoJson()} style={this.getStyle} />
+    }
+
     return (
       <div className="App-container">
         <div className="App-info">
           <h1>Reporte ANP</h1>
         </div>
+        <Map 
+            className="App-map"
+            center={position} 
+            zoom={zoom} 
+            maxZoom={15} 
+            minZoom={3}>
+            <TileLayer
+              attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+              url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}' />
+            {geom}
+        </Map>
         <Desktop>
           <div className="App-list">
             <ul>
@@ -27,5 +84,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
