51230111fd028afae67945970e52ac59c6160106
amaurs <amaury.gtz@gmail.com>
2018-07-24 13:19:10 -0500

    The windows can be opened and closed.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import * as turf from '@turf/turf';
 
 const Desktop = props => <Responsive {...props} minWidth={992} />;
+
+
+const breakpoints = {
+  desktop: 992,
+  tablet: 768
+};
+
 //const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
 //const Mobile = props => <Responsive {...props} maxWidth={767} />;
 //const Default = props => <Responsive {...props} minWidth={768} />;
@@ -19,7 +26,8 @@ class App extends Component {
       geojson: null,
       ready: false,
       boundBox: null,
-
+      listIsActive:true,
+      infoIsActive:true
     };
   }
 
@@ -64,49 +72,68 @@ class App extends Component {
     this.getBoundingBoxFromMap();
   }
 
+  handleButtonHideList(event) {
+    console.log("Button was clicked");
+    let listIsActive = this.state.listIsActive;
+    this.setState({listIsActive:!listIsActive});
+  }
+
+  handleButtonHideInfo(event) {
+    console.log("Button was clicked");
+    let infoIsActive = this.state.infoIsActive;
+    this.setState({infoIsActive:!infoIsActive});
+  }
+
   getBoundingBoxFromMap() {
     let bounds = this.leafletMap.leafletElement.getBounds();
-    let boundBox = [[
-                    [bounds.getWest(), bounds.getNorth()],
-                    [bounds.getEast(), bounds.getNorth()],
-                    [bounds.getEast(), bounds.getSouth()],
-                    [bounds.getWest(), bounds.getSouth()],
-                    [bounds.getWest(), bounds.getNorth()]
-                   ]];
-    let alternative = [bounds.getWest(), bounds.getNorth(), bounds.getEast(), bounds.getSouth()];
-    this.setState({boundBox:alternative});
+    let boundBox = [bounds.getWest(), 
+                       bounds.getNorth(), 
+                       bounds.getEast(), 
+                       bounds.getSouth()];
+    this.setState({boundBox: boundBox});
   }
 
   render() {
     let geom = null;
     let list = null;
-    if (this.state.ready) {
+    if(this.state.ready) {
       console.log("I am ready to paint!");
       geom = <GeoJSON data={this.getGeoJson()} style={this.getStyle} />
-      list = this.getList();
+      if(window.innerWidth > breakpoints.desktop) {
+        console.log("Filtering list of polygons.");
+        list = this.getList();
+      }
     }
 
     return (
       <div className="App-container">
-        <div className="App-info">
+        <div className={"App-info" + (this.state.infoIsActive? "": " is-inactive")}>
           <h1>Reporte ANP</h1>
         </div>
-        <Map 
-            className="App-map"
-            center={position} 
-            ref={map => { this.leafletMap = map; }} 
-            zoom={zoom} 
-            maxZoom={15} 
-            minZoom={3}
-            onZoom={(e)=>this.handleBoundingBoxChange(e)} 
-            onMoveend={(e)=>this.handleBoundingBoxChange(e)} >
-            <TileLayer
-              attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
-              url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}' />
-            {geom}
-        </Map>
+        <div>
+          <button 
+            className="App-list-button"
+            onClick={(e)=>this.handleButtonHideList(e)}>&gt;</button>
+          <button 
+            className="App-info-button"
+            onClick={(e)=>this.handleButtonHideInfo(e)}>&gt;</button>
+          <Map 
+              className="App-map"
+              center={position} 
+              ref={map => { this.leafletMap = map; }} 
+              zoom={zoom} 
+              maxZoom={15} 
+              minZoom={3}
+              onZoom={(e)=>this.handleBoundingBoxChange(e)} 
+              onMoveend={(e)=>this.handleBoundingBoxChange(e)} >
+              <TileLayer
+                attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+                url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}' />
+              {geom}
+          </Map>
+        </div>
         <Desktop>
-          <div className="App-list">
+          <div className={"App-list" + (this.state.listIsActive? "": " is-inactive")}>
             <ul>
               {list}
             </ul>
