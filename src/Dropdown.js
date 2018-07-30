c5399a8cc195a2f175242c8dedb6f6f19110455a
amaurs <amaury.gtz@gmail.com>
2018-07-30 16:45:08 -0500

    Created components for option lists.

---
+++ src/Dropdown.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class List extends Component {
+
+    constructor(props) {
+      super(props);
+      this.handleClick = this.props.handleClick;
+    }
+
+    render() {
+      const code = this.props.code;
+      const name = this.props.name;
+      const anps = this.props.anps.map((element, index) => <option key={index} 
+                                                                   value={element.properties[code]}>{element.properties[name]}</option>);
+      return <select className="navbar-item" 
+                     onChange={e=>this.handleClick(e)}>{anps}</select>;
+    }
+}
