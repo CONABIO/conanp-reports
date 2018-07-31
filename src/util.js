b58f13c1682f87d8ce04dc67ba810dbffdf91d8b
amaurs <amaury.gtz@gmail.com>
2018-07-31 13:17:16 -0500

    Fixed dropdown list UI for mobile.

--- src/util.js
+++ src/util.js
@@ -3,11 +3,11 @@ const breakpoints = {
   tablet: 768
 };
 
-//const API = "http://127.0.0.1:8080/anps.geojson";
-const API = "http://snmb.conabio.gob.mx/api_anps/v1/anps"
-//const CODE = "ID_07";
-const CODE = "id_07";
-//const NAME = "NOMBRE";
-const NAME = "nombre";
+const API = "http://127.0.0.1:8080/anps.geojson";
+//const API = "http://snmb.conabio.gob.mx/api_anps/v1/anps"
+const CODE = "ID_07";
+//const CODE = "id_07";
+const NAME = "NOMBRE";
+//const NAME = "nombre";
 
 export { breakpoints, API, CODE, NAME };
